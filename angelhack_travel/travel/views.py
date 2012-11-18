9b5c3abfe6efc6d6913e6e9c7d855f02f0018b5d
Lucas Lemanowicz <lemanowiczlucas@gmail.com>
2012-11-17 23:41:41 -0500

    Fixing bug in Singly-provided code.

--- angelhack_travel/travel/views.py
+++ angelhack_travel/travel/views.py
@@ -6,7 +6,6 @@ from models import Trip
 
 import simplejson
 
-# Main application page
 def home(request, template='index.html'):
     services = [
         'Facebook',
@@ -18,19 +17,11 @@ def home(request, template='index.html'):
         'FitBit',
         'Email'
     ]
+
     if request.user.is_authenticated():
         user_profile = request.user.get_profile()
-        # We replace single quotes with double quotes b/c of python's strict json requirements
-        profiles = simplejson.loads(user_profile.profiles.replace("'", '"'))
+
     response = render_to_response(
             template, locals(), context_instance=RequestContext(request)
         )
     return response
-
-# Returns all trips in json format
-def trips(request):
-  pass
-
-# Returns all events associated with a trip in json format
-def events(request, trip_id=None):
-  pass
